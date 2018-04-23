2eb95de75c03cd4e61910536252dcff6d11873d6
Vladimir Gavrilov <vgavrilov@diatomenterprises.com>
2018-04-23 17:36:08 +0300

    Auto-generated commit

--- src/app/locations-filter.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { LocationsFilterPipe } from './locations-filter.pipe';
-
-describe('LocationsFilterPipe', () => {
-  it('create an instance', () => {
-    const pipe = new LocationsFilterPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
